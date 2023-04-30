eed29864cc5b669d2a5c34276db0d16298e3cb21
Leandro Gomes Cardoso <leanddro.cardoso@gmail.com>
2023-04-30 20:57:38 -0300

    Testes de edição de fornecedor e de detalhes

---
+++ spec/system/suppliers/user_view_supplier_details_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe 'Usuário vê detalhes do fornecedor', type: :feature do
+  it 'a partir da tela inicial' do
+    Supplier.create!(corporate_name: 'ACME LTDA', brand_name: 'ACME',
+                    registration_number: '1880678200011', full_address: 'Av das Palmas, 100',
+                    city: 'Bauru', state: 'SP', email: 'contato@acme.com')
+
+    visit root_path
+    click_on 'Fornecedores'
+    click_on 'ACME'
+
+    expect(page).to have_content 'ACME LTDA'
+    expect(page).to have_content 'Documento: 1880678200011'
+    expect(page).to have_content 'Endereço: Av das Palmas, 100 - Bauru - SP'
+    expect(page).to have_content 'E-mail: contato@acme.com'
+  end
+  it 'e volta para tela inicial' do
+    Supplier.create!(corporate_name: 'ACME LTDA', brand_name: 'ACME',
+                    registration_number: '1880678200011', full_address: 'Av das Palmas, 100',
+                    city: 'Bauru', state: 'SP', email: 'contato@acme.com')
+
+    visit root_path
+    click_on 'Fornecedores'
+    click_on 'ACME'
+    click_on 'Voltar'
+
+    expect(page).to have_content 'Fornecedores'
+  end
+
+end
+
