cb4ccb120b40cb9ffc729d15ccd7090e5fb0e021
Leandro Gomes Cardoso <leanddro.cardoso@gmail.com>
2023-04-30 20:23:36 -0300

    Adicionadno rotas e ações de cadastro de novo fornecedor

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,5 +1,32 @@
 class SuppliersController < ApplicationController
+  before_action :set_supplier, only: %i(show)
   def index
     @suppliers = Supplier.all
+  end
+
+  def new
+    @supplier = Supplier.new
+  end
+
+  def create
+    @supplier = Supplier.new(supplier_params)
+
+    return redirect_to @supplier, notice: 'Fornecedor cadastrado com sucesso' if @supplier.save
+
+    flash[:alert] = 'Fornecedor não cadastrado'
+    render 'new'
+  end
+
+  def show; end
+
+  private
+
+  def set_supplier
+     @supplier = Supplier.find(params[:id])
+  end
+
+  def supplier_params
+    params.require(:supplier)
+          .permit(:corporate_name, :brand_name, :registration_number, :full_address, :city, :state, :email)
   end
 end
