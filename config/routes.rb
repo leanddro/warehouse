b09122760ddbdb2001275023214b4dcb6046b31d
Leandro Gomes Cardoso <leanddro.cardoso@gmail.com>
2023-05-02 18:49:56 -0300

    Adiconado rotas, adicionado tradução do model

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   resources :warehouses, only: %i(show new create edit update destroy)
   resources :suppliers, only: %i(index new create show edit update)
+  resources :product_models, only: %i(index new create show)
 end
